08d68b7a795532532b152440b2a4a00a8417bc94
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-10-19 16:34:44 -0500

    added Hotels table

--- AsyncInn/Data/AsyncInnDbContext.cs
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using AsyncInn.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AsyncInn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
-        public AsyncInnDbContext(DbContextOptions options) : base(options)
-        {
-        }
+        public AsyncInnDbContext(DbContextOptions options) : base(options){}
+
+        public DbSet<Hotel> Hotels { get; set; }
     }
 }
