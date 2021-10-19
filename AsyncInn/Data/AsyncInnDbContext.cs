f9bcd241c0e915bad055ff58c14f36769af2ca48
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-10-19 16:32:24 -0500

    added Hotel.cs and DbSet

---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
